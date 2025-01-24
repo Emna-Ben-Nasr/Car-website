d1c6266d44106b009d390b11ca79970005684ed4
Emna Ben Nasr <emnabennasr20@gmail.com>
2025-01-24 10:51:06 +0100

    creating car types and fuel types tables ,modifiying users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone', length: 45)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('facebook_id', length: 45)->unique()->nullable();
+            $table->string('google_id', length: 45)->unique()->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
